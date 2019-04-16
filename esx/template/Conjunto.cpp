8da0dc2232d182b43b9ed4db0389523b900cb9d5
Kaio Henrique De Sousa <kaiohenrique@A204-03L.imd.local>
2019-04-16 14:48:13 -0300

    template

--- esx/template/Conjunto.cpp
+++ esx/template/Conjunto.cpp
@@ -1,2 +1,181 @@
-#include "Conjunto.h"
+#ifndef CONJUNTO_H
+#define CONJUNTO_H
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+template<typename T>
+class conjunto{
+private:
+	vector<T> conj;
+public:
+	conjunto();
+	conjunto(const conjunto& c);
+	~conjunto();
+	T getconj();
+
+	void add(T t);
+
+	void add(conjunto& c);
+
+	int Total()const;
+
+	template<typename t>
+	friend ostream& operator<<(ostream& o, const conjunto<t>& c);
+
+	T operator[](int indice)const;
+
+	conjunto<T> operator+(conjunto& c)const;
+
+	conjunto<T> operator*(conjunto& c)const;
+
+	conjunto<T> operator/(conjunto& c)const;
+
+	conjunto<T> operator-(conjunto& c)const;
+};
+
+template<typename T>
+conjunto<T>::conjunto(){}
+
+template<typename T>
+conjunto<T>::conjunto(const conjunto& c): conj(c.conj){}
+
+template<typename T>
+conjunto<T>::~conjunto(){ conj.clear(); }
+
+template<typename T>
+T conjunto<T>::getconj(){
+	return conj;
+}
+
+template<typename T>
+void conjunto<T>::add(T t){
+	conj.push_back(t);
+}
+
+template<typename T>
+void conjunto<T>::add(conjunto& c){
+	for(int i = 0; i<c.Total(); i++){
+		conj.push_back(c[i]);
+	}
+}
+
+template<typename T>
+int conjunto<T>::Total()const{
+	return conj.size();
+}
+
+template<typename t>
+ostream& operator<<(ostream& o, const conjunto<t>& c){
+	for(int i = 0; i<(int)c.conj.size(); ++i){
+		o<<c.conj[i]<<" ";
+	}
+	o<<endl;
+	return o;
+}
+
+template<typename T>
+T conjunto<T>::operator[](int indice)const{
+	return conj[indice];
+}
+
+template<typename T>
+conjunto<T> conjunto<T>::operator+(conjunto& c)const{
+	conjunto<T> aux;
+	int tam = conj.size();
+	int tam_c = c.conj.size();
+	aux.add(c);
+
+	int j = 0;
+	for(int i=0; i<tam; i++){
+
+		while(j < tam_c && conj[i] != c[j]){
+			j++;
+		}
+		if(j == tam_c){
+			aux.add(conj[i]);
+		}
+		j = 0;
+	}
+	return aux;
+}
+
+
+template<typename T>
+conjunto<T> conjunto<T>::operator*(conjunto& c)const{
+	conjunto<T> aux;
+	int tam = conj.size();
+	int tam_c = c.conj.size();
+
+	int j = 0;
+	for(int i=0; i<tam; i++){
+
+		while(j < tam_c){
+			if(conj[i] == c[j]){
+				aux.add(conj[i]);
+			}
+			j++;
+		}
+
+		j = 0;
+	}
+	return aux;
+}
+
+template<typename T>
+conjunto<T> conjunto<T>::operator/(conjunto& c)const{
+	conjunto<T> aux;
+	int tam = conj.size();
+	int tam_c = c.conj.size();
+
+	int j = 0;
+	for(int i=0; i<tam; i++){
+		aux.add(conj[i]);
+		while(j < tam_c){
+			if(conj[i] == c[j]){
+				aux.conj.pop_back();
+			}
+			j++;
+		}
+		j = 0;
+	}
+	return aux;
+}
+
+template<typename T>
+conjunto<T> conjunto<T>::operator-(conjunto& c)const{
+	conjunto<T> aux;
+	int tam = conj.size();
+	int tam_c = c.conj.size();
+
+	int j = 0;
+	for(int i=0; i<tam; i++){
+		aux.add(conj[i]);
+		while(j < tam_c){
+			if(conj[i] == c[j]){
+				aux.conj.pop_back();
+			}
+			j++;
+		}
+		j = 0;
+	}
+
+	j = 0;
+	for(int i=0; i<tam_c; i++){
+		aux.add(c[i]);
+		while(j < tam){
+			if(c[i] == conj[j]){
+				aux.conj.pop_back();
+			}
+			j++;
+		}
+		j = 0;
+	}
+
+	return aux;
+}
+
+
+
+#endif
--- esx/template/main.cpp
+++ esx/template/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
-#include "Conjunto.h"
+#include "Conjunto.cpp"
 
 int main (int argv, char const *argc[]){
 	conjunto<int> A;
+	conjunto<int> B;
+	conjunto<int> C;
+
+	B.add(1); B.add(2); B.add(3); B.add(4); B.add(5); B.add(6); B.add(7);
+	C.add(4); C.add(5); C.add(6); C.add(7); C.add(8); C.add(9); C.add(10);
 
     for(int i = 0; i<10; ++i)
 		A.add(i);
-	A.imprimir();
+	cout<<A;
+
+	conjunto<int> S = B-C;
+	cout<<S;
 
     cout << "\n\nProgram ends successfully!" <<endl;
 }
